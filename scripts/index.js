6831519ade9d0d60a876005b4f848535612a4794
Ileenfdz <ileenfdz@gmail.com>
2021-12-19 18:53:16 +0100

    Modified the comments in index.js to clarify what should be the output of each one

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,17 @@ import {addBinary} from './extrax2exercise.js';
 
 export function app(){
     // console.log("Connected to app");
-    uniTotal("a"); //97
-    uniTotal("b"); //98
-    uniTotal("c"); //99
-    uniTotal("d"); //100
-    uniTotal("e"); //101
+    uniTotal("a"); //Result should be = 97
+    uniTotal("b"); //Result should be = 98
+    uniTotal("c"); //Result should be = 99
+    uniTotal("d"); //Result should be = 100
+    uniTotal("e"); //Result should be = 101
 
-    elevatorDistance([5,2,8]); //9
-    elevatorDistance([1,2,3]); //2
-    elevatorDistance([7,1,7,1]); //18
+    elevatorDistance([5,2,8]); //Result should be = 9
+    elevatorDistance([1,2,3]); //Result should be = 2
+    elevatorDistance([7,1,7,1]); //Result should be = 18
 
-    addBinary(1,1); //10
-    addBinary(5,9); //1110
+    addBinary(1,1); //Result should be = 10
+    addBinary(5,9); //Result should be = 1110
 }
 app();
