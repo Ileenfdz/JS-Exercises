7d7be5dec06a0ce772b6cdd1e4b2f5d9f91f6495
Ileenfdz <ileenfdz@gmail.com>
2021-12-29 02:27:22 +0100

    Added new exercise and it's modifications in Readme and index.html

--- scripts/exercise.js
+++ scripts/exercise.js
@@ -1,24 +1,10 @@
-export function numberToString(num) {
-    console.log(num.toString());
-}
+export function squareSum(numbers){
 
-export function removeChar(str){console.log(str.slice(1,-1))}
-
-export function findSmallestInt(args) {
-    let small = 0;
-    for(let i = 0;i < args.length;i++){
-      if(args[i] <= args[0]){
-        args[0]  = args[i];    
-        small = args[i];
-      }
+  var total = 0;
+  
+    for(var i = 0; i < numbers.length; i++) {
+      total += numbers[i] * numbers[i]; 
     }
-    console.log(small);
-}
-
-export function summation(num) {
-    let sum = 0;
-    for(let i = 0; i <= num; i++) {
-        sum += i;
-    }
-    console.log(sum);
+  
+  console.log(total);
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,8 @@
-import { numberToString, removeChar, findSmallestInt, summation } from './exercise.js';
+import { squareSum} from './exercise.js';
 export function app(){
     // console.log("Connected to app");
-    numberToString(42)//Result should be = "42"
-    numberToString(5)//Result should be = "5"
-    numberToString(20)//Result should be = "20"
-
-    removeChar("hello")//Result should be = "ell"
-    removeChar("Goodbye")//Result should be = "oodby"
-    removeChar("Happy")//Result should be = "app"
-
-    findSmallestInt([20,3,500,2,1])//Result should be = "1"
-    findSmallestInt([20,10,62,9,12])//Result should be = "9"
-    findSmallestInt([21,31,5,23,14])//Result should be = "5"
-
-    summation(20)//Result should be = "210"
-    summation(2)//Result should be = "3"
-    summation(8)//Result should be = "36"
+    squareSum([1,2])//Result should be = "5"
+    squareSum([0,3,4,5])//Result should be = "50"
+    squareSum([0])//Result should be = "0"
 }
 app();
