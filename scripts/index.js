9f2157192ed816810670191c318c4c5795edfc29
Ileenfdz <ileenfdz@gmail.com>
2022-01-03 20:03:45 +0100

    Updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import { shortcut } from './exercise.js';
-export function app(){
+import { shortcut } from './exercise17.js';
+export function app17(){
     // console.log("Connected to app");
     shortcut('hello')//Result should be = 'hll'
     shortcut('how are you today?')//Result should be = 'hw r y tdy?
