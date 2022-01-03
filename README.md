0a38cccfbeec70d285d529e7184a483414c52312
Ileenfdz <ileenfdz@gmail.com>
2022-01-03 19:54:48 +0100

    Updated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Daily exercise using Js -> Find the odd int total 6 Kyu.
 
-## Description
+## Description(8)
 
 Very simple, given an integer or a floating-point number, find its opposite.
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import { opposite } from './exercise.js';
-export function app(){
+import { opposite } from './exercise8.js';
+export function app8(){
     // console.log("Connected to app");
     opposite(1)//Result should be = -1
     opposite(14)//Result should be = -14
