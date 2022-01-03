2be7e5ef3587240f65a266995a00f5930439790e
Ileenfdz <ileenfdz@gmail.com>
2022-01-03 11:59:16 +0100

    Added extra exercise

--- scripts/exercise.js
+++ scripts/exercise.js
@@ -1,3 +1,7 @@
 export function basicOp(operation, value1, value2){
     console.log(eval(value1+operation+value2));
 }
+
+export function reverseWords(str) {
+    console.log(str.split(' ').reverse().join(' '));
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
-import { basicOp } from './exercise.js';
+import { basicOp, reverseWords } from './exercise.js';
 export function app(){
     // console.log("Connected to app");
     basicOp('+', 4, 7)//Result should be = 11
     basicOp('-', 15, 18)//Result should be = -3
+
+    reverseWords("hello world!")//Result should be = "world! hello"
+    reverseWords("yoda doesn't speak like this")//Result should be = "this like speak doesn't yoda"
 }
 app();
