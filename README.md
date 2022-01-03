a8d369f39cd31564ef7415bc7878dce7d5c08ae1
Ileenfdz <ileenfdz@gmail.com>
2022-01-03 19:53:43 +0100

    Updated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Daily exercise using Js -> Find the odd int total 6 Kyu.
 
-## Description
+## Description(7)
 
 Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import { paperwork } from './exercise.js';
-export function app(){
+import { paperwork } from './exercise7.js';
+export function app7(){
     // console.log("Connected to app");
     paperwork(5,5)//Result should be = 25
     paperwork(0,5)//Result should be = 0
