ce4b61dab9f64bbbe482d78bf1e1e57448f0aa6a
Ileenfdz <ileenfdz@gmail.com>
2022-01-03 20:00:44 +0100

    Updated

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Daily exercise using Js -> Find the odd int total 6 Kyu.
 
-## Description
+## Description(14)
 
 The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import { century } from './exercise.js';
-export function app(){
+import { century } from './exercise14.js';
+export function app14(){
     // console.log("Connected to app");
     century(1780)//Result should be = "18"
     century(1970)//Result should be = "20"
