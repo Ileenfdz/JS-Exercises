6dc81cf36a8ed19e973444c1a756608b8fa934c9
Ileenfdz <ileenfdz@gmail.com>
2022-01-03 20:00:06 +0100

    Updated

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Daily exercise using Js -> Find the odd int total 6 Kyu.
 
-## Description
+## Description(13)
 
 Nathan loves cycling.
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import { litres } from './exercise.js';
-export function app(){
+import { litres } from './exercise13.js';
+export function app13(){
     // console.log("Connected to app");
     litres(2)//Result should be = "1"
     litres(12.3)//Result should be = "6"
